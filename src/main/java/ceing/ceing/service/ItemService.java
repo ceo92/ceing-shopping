7a120e894cd8c1bd77c91a053922bbb73749cd42
ceo92 <king9243@naver.com>
2024-07-17 08:56:05 +0900

    상품 목록 타임리프 마이그레이션 전

--- src/main/java/ceing/ceing/service/ItemService.java
+++ src/main/java/ceing/ceing/service/ItemService.java
@@ -1,5 +1,8 @@
 package ceing.ceing.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ItemService {
 
 }
--- src/main/java/ceing/ceing/web/controller/ItemController.java
+++ src/main/java/ceing/ceing/web/controller/ItemController.java
@@ -1,11 +1,29 @@
 package ceing.ceing.web.controller;
 
 import ceing.ceing.service.ItemService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class ItemController {
 
-  //private final ItemService itemSerivce;
+  private final ItemService itemSerivce;
+
+  /**
+   * 상품 목록
+   */
+
+  @GetMapping("items")
+  public String aa(){
+    return "item/items";
+  }
+
+
+
+  /**
+   * 상품
+   */
 
 }
